fec2ae19dd9676be03f754ea8c76be4e91a90c42
manisha7903 <64511542+manisha7903@users.noreply.github.com>
2020-05-22 22:06:14 +0530

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -94,3 +94,32 @@ $(document).on("click", '[data-toggle="lightbox"]', function(event) {
   event.preventDefault();
   $(this).ekkoLightbox();
 });
+// Set the date we're counting down to
+    var countDownDate = new Date("April 10, 2020 21:00:00").getTime();
+
+    // Update the count down every 1 second
+    var x = setInterval(function() {
+        // Get todays date and time
+        var now = new Date().getTime();
+        // Find the distance between now an the count down date
+        var distance = countDownDate - now;
+        // Time calculations for days, hours, minutes and seconds
+        var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+        var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+        // Output the result in an element with id="demo"
+        document.getElementById("days").innerHTML = days;
+        document.getElementById("hours").innerHTML = hours;
+        document.getElementById("minutes").innerHTML = minutes;
+        document.getElementById("seconds").innerHTML = seconds;
+
+        document.getElementById("all").innerHTML = days + ": " + hours + ": " + minutes + ": " + seconds;
+
+        // If the count down is over, write some text 
+        if (distance < 0) {
+            clearInterval(x);
+            document.getElementById("all").innerHTML = "EXPIRED";
+        }
+    }, 1000);
